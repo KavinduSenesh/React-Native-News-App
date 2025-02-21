e76136152ea00131d412cf6630fea6e49356551e
Kavindu Senesh <ksenesh5@gmail.com>
2025-02-21 21:37:49 +0530

    Add CheckBox component and integrate category selection in Discover page

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,23 +1,40 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import SearchBar from "@/components/SearchBar";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import newsCategoryList from "@/constants/Categories";
+import CheckBox from "@/components/CheckBox";
+import {useNewsCategories} from "@/hooks/useNewsCategories";
 
 type Props = {}
 
 const Page = (props: Props) => {
   const {top: safeTop} = useSafeAreaInsets();
 
+  const {newsCategories, toggleNewsCategory} = useNewsCategories();
+
+
   return (
     <View style={[styles.container, {paddingTop: safeTop + 20}]}>
       <SearchBar withHorizontalPadding={false}/>
       <Text style={styles.title}>Categories</Text>
       <View style={styles.listContainer}>
-        {newsCategoryList.map((item) => (
-          <Text>{item.title}</Text>
+        {newsCategories.map((item) => (
+          <CheckBox key={item.id} label={item.title} checked={item.selected} onPress={() => {
+            toggleNewsCategory(item.id)
+          }}/>
         ))}
       </View>
+
+      <Text style={styles.title}>Country</Text>
+      <View style={styles.listContainer}>
+        {newsCategories.map((item) => (
+            <CheckBox key={item.id} label={item.title} checked={item.selected} onPress={() => {
+              toggleNewsCategory(item.id)
+            }}/>
+        ))}
+      </View>
+
     </View>
   )
 }
